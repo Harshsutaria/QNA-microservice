13c68ba9fd4299b9fde4ed644884b175b2f4e80e
Harsh Sutaria <harsh.sutaria@tapclicks.com>
2024-03-16 02:31:31 +0530

    Adding some utils

---
+++ utils/interfaces/QuestionInterface.ts
@@ -0,0 +1,9 @@
+export default interface QuestionInterface {
+  questionId: string;
+  questionText: string;
+  teacherId: string;
+  isActive: boolean;
+  ttl: string;
+  createdTS: string;
+  updatedTS: string;
+}
