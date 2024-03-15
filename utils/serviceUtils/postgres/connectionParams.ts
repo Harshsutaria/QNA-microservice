644fb8dab2dea236b0917b4f4eb9f5cd75734510
Harsh Sutaria <harsh.sutaria@tapclicks.com>
2024-03-16 00:10:04 +0530

    Adding some utils

---
+++ utils/serviceUtils/postgres/connectionParams.ts
@@ -0,0 +1,15 @@
+import PostgresConnectionParamsInterface from "../../interfaces/postgresConnectionParamsInterface";
+
+/**
+ * Intentionally kept wrong constants
+ * Please shoot an email to harshsutaria25@gmail.com for the correct creds.
+ */
+const postgresConnectionParams: PostgresConnectionParamsInterface = {
+  host: "3.7.44.41",
+  port: 5432,
+  database: "commondb",
+  user: "openerp",
+  password: "openerp",
+};
+
+export default postgresConnectionParams;
